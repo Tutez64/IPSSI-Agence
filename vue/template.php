3450cfef7b5136882d51edb602c5ba77f77e7eca
tutez-64 <enzopersillet@gmail.com>
2024-11-23 21:59:45 +0100

    Modals signin/signup

--- vue/template.php
+++ vue/template.php
@@ -1,3 +1,4 @@
+<?php ob_start(); ?>
 <!DOCTYPE html>
 <html lang="fr" data-bs-theme="dark">
 <head>
@@ -12,14 +13,14 @@
 <header class="bg-secondary p-4 mb-3">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Agence</a>
+            <a class="navbar-brand" href="index.php">Agence</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
@@ -36,13 +37,13 @@
                         </ul>
                     </li>
                 </ul>
-                <button type="button" onclick="window.location.href='signup.php';" class="btn btn-outline-secondary" style="margin: 10px;">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#signupModal" style="margin: 10px;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                     </svg>
                     Créer un compte
                 </button>
-                <button type="button" onclick="window.location.href='signin.php';" class="btn btn-outline-secondary">
+                <button type="button" class="btn btn-outline-secondary"  data-bs-toggle="modal" data-bs-target="#signinModal" style="margin: 10px;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
                         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
@@ -50,6 +51,27 @@
                         Se connecter
                 </button>
             </div>
+
+            <div class="modal fade" id="signinModal" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <?php include "signin.html" ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="signupModal" tabindex="-2" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-body">
+					        <?php include "signup.html" ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </nav>
 </header>
