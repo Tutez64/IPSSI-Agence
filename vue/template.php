9c9678d69907b5d7544bff2513d3e0d7c1a7317b
tutez-64 <enzopersillet@gmail.com>
2024-11-23 14:29:46 +0100

    Add template

---
+++ vue/template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Agence</title>
+</head>
+<body>
+
+<header class="bg-secondary p-4 mb-3"></header>
+
+<main class="container-fluid">
+	<?= $contenu ?? ''; ?>
+</main>
+
+<footer class="bg-secondary p-4 text-center text-light mt-4">
+    Enzo PERSILLET - 2024
+</footer>
+
+</body>
+</html>
